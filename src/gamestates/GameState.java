0e851b2778e0aeec0158814795240574c8c667e4
Minchh <minh.nguyendo3103@gmail.com>
2023-05-17 22:17:12 +0700

    Added Gamestate and resources

---
+++ src/gamestates/GameState.java
@@ -0,0 +1,11 @@
+package gamestates;
+
+public enum GameState
+{
+	PLAYING,
+	MENU,
+	OPTIONS,
+	QUIT;
+
+	public static GameState state = MENU;
+}
---
+++ src/gamestates/StateMethods.java
@@ -0,0 +1,9 @@
+package gamestates;
+
+import java.awt.Graphics;
+
+public interface StateMethods
+{
+	public void update();
+	public void render(Graphics g);
+}
