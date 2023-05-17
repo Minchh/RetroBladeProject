7a8b4f6ccb0e858d88fe0b481761d3b31ec07d3d
Minchh <minh.nguyendo3103@gmail.com>
2023-05-17 09:50:10 +0700

    Added Entity class

---
+++ src/entities/Entity.java
@@ -0,0 +1,72 @@
+package entities;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.geom.Rectangle2D;
+
+public class Entity
+{
+	private float x, y;
+	private int width, height;
+	private Rectangle2D.Float rect;
+
+	public Entity(float x, float y, int width, int height)
+	{
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	// For debugging the hitbox;
+	protected void renderRect(Graphics g, int xLvlOffset)
+	{
+		g.setColor(Color.RED);
+		g.drawRect((int)rect.x - xLvlOffset, (int)rect.y, (int)rect.width, (int)rect.height);
+		g.setColor(Color.BLACK);
+	}
+
+	protected void initRect(float x, float y, float width, float height) {
+		rect = new Rectangle2D.Float(x, y, width, height);
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public Rectangle2D.Float getRect() {
+		return rect;
+	}
+
+	public void setRect(Rectangle2D.Float rect) {
+		this.rect = rect;
+	}
+}
